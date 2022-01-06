d9a75e2da313c0e67938911b926d649cb1c52db9
luky <lindel33@mail.ru>
2022-01-06 23:22:33 +0300

    Чистка

--- repair/repair_home/urls.py
+++ repair/repair_home/urls.py
@@ -1,13 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
-from .views import index, Home, Detail
+from django.urls import path
+from .views import Home, Detail
 
 urlpatterns = [
     path('', Home.as_view()),
-    # path('delivery', )
-    # path('products', products)
     path('about_us', Home.as_view()),
     path('detail/<str:pk>', Detail.as_view(), name='product'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
